92042bf1f5c833feed0d0a8aec0f3c1aa8f8bfec
Hasung Jun <59468036+kimchidad@users.noreply.github.com>
2020-05-19 11:15:57 +0900

    Weekly Reminder (Pop up alert on next buttons when user didn't choose the option)

--- LaundryMan/alarm.js
+++ LaundryMan/alarm.js
@@ -4,6 +4,69 @@ $(document).ready(function(){
   .dropdown();
   $('.ui.accordion')
   .accordion();
+  $('.ui.modal')
+  .modal();
+
+//   Alert js
+  $.uiAlert = function(options) {
+    var setUI = $.extend({
+    textHead: 'Your user registration was successful.',
+    text: 'You may now log-in with the username you have chosen',
+    textcolor: '#19c3aa',
+    bgcolors: '#fff',
+    position: 'top-right',
+    icon: '',
+    time: 5,
+    permanent: false
+    }, options);
+
+    var ui_alert = 'ui-alert-content';
+    ui_alert += '-' + setUI.position;
+    setUI.bgcolors ='style="background-color: '+setUI.bgcolor+';   box-shadow: 0 0 0 1px rgba(255,255,255,.5) inset,0 0 0 0 transparent;"';
+    if(setUI.bgcolors === '') setUI.bgcolors ='style="background-color: ; box-shadow: 0 0 0 1px rgba(255,255,255,.5) inset,0 0 0 0 transparent;"';
+    if(!$('body > .' + ui_alert).length) {
+        $('body').append('<div class="ui-alert-content ' + ui_alert + '" style="width: inherit;"></div>');
+    }
+    var message = $('<div id="messages" class="ui icon message" ' + setUI.bgcolors + '><i class="'+setUI.icon+' icon" style="color: '+setUI.textcolor+';"></i><i class="close icon" style="color: '+setUI.textcolor+';" id="messageclose"></i><div style="color: '+setUI.textcolor+'; margin-right: 10px;">   <div class="header">'+setUI.textHead+'</div>  <p> '+setUI.text+'</p></div>  </div>');
+    $('.' + ui_alert).prepend(message);
+    message.animate({
+        opacity: '1',
+    }, 300);
+    if(setUI.permanent === false){
+        var timer = 0;
+        $(message).mouseenter(function(){
+        clearTimeout(timer);
+        }).mouseleave(function(){
+        uiAlertHide();
+        });
+        uiAlertHide();
+    }
+    function uiAlertHide(){
+        timer = setTimeout(function() {
+            message.animate({
+                opacity: '0',
+            } , 300, function() {
+                message.remove();
+        });
+        }, (setUI.time * 1000) );
+    }
+
+    $('#messageclose').on('click', function() {
+        $(this).closest('#messages').transition('fade');
+    });
+  };
+  function triggerAlert (option) {
+        $.uiAlert({
+            textHead: "Please select the " + option + "!", // header
+            text: '', // Text
+            bgcolor: '#DB2828', // background-color
+            textcolor: '#fff', // color
+            position: 'bottom-right',// position . top And bottom ||  left / center / right
+            icon: 'remove circle', // icon in semantic-UI
+            time: 3, // time
+        });
+   };
+  
   //True meaning button is clicked
   //False meaning the opposite
   function checkStates () {
@@ -14,6 +77,8 @@ $(document).ready(function(){
       }
       return false;
   }
+
+  //Find index which button is clicked in states
   function findIndex (j) {
       for (i = 0; i < states.length; i++) {
           if (states[i] && i != j) {
@@ -25,6 +90,7 @@ $(document).ready(function(){
   
   //True meaning button is clicked
   //False meaning the opposite
+  //Change color of each button based on user's click
   $("#mon").on("click", (function() {
       if (states[0] == true) {
           $(this).css("background-color", "#35B3FA");
@@ -167,11 +233,25 @@ $(document).ready(function(){
           else {                  $("#sat").css("background-color", "#35B3FA"); states[i] = false; }
       }
   })
+
+  $(".title").click(function() {
+    
+  })
   $("#daynext").click(function() {
-    $('.ui.accordion').accordion('open', 1);
+    if (!checkStates()) {
+        triggerAlert("day");
+    }
+    else {
+        $('.ui.accordion').accordion('open', 1);
+    }
   })
   $("#timenext").click(function() {
-    $('.ui.accordion').accordion('open', 2);
+    if (!checkStates()) {
+        triggerAlert("time");
+    }
+    else {
+        $('.ui.accordion').accordion('open', 2);
+    }
   })
   $("#timeprev").click(function() {
     $('.ui.accordion').accordion('open', 0);
@@ -179,8 +259,14 @@ $(document).ready(function(){
   $("#howprev").click(function() {
     $('.ui.accordion').accordion('open', 1);
   })
-  var modal = document.getElementById("myModal");
+  
   $("#savehow").click(function() {
-    modal.style.display = "block";
+    $('#myModal').modal('show');
+  })
+  $("#noModalbtn").click(function() {
+    $('#myModal').modal('hide');
+  })
+  $("#yesModalbtn").click(function() {
+    window.location.href="index.html";
   })
 });
